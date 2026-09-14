325d2bbfb1632bb49c8d7ab47c60dc004525e1fb
Divyanshi Saxena <saxenadivyanshi950@gmail.com>
2026-09-14 21:43:29 +0530

    Add Linear search code by making method

---
+++ src/LinearSearch/CodeLinearSearchMethod.java
@@ -0,0 +1,28 @@
+package LinearSearch;
+
+public class CodeLinearSearchMethod {
+    static void main() {
+        int[] nums = {23,45,1,2,19,-3};
+        int target = 19;
+        int ans = linearSearch(nums,target);
+        System.out.println(ans);
+    }
+    //search in the array: return the index if item found
+    //otherwise if item not found return -1
+    static int linearSearch(int[] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+        // run a for loop
+        for(int index = 0; index<arr.length; index++){
+            // check for element at every index if it is = target
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        // this line will execute if none of the return statement above have executed
+        // hence the target not found
+        return -1; // or return Integer.MAX_VALUE; if -1 is in the array then how will someone know that it is in the list or not.
+    }
+}
